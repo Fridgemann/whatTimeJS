17d5ffc07214f26a9bba9420e7c910bd12a73656
Fridgemann <120120633+Fridgemann@users.noreply.github.com>
2025-02-13 00:36:07 +0300

    dynamic clock working

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
 const dayjs = require('dayjs');
+const utc = require('dayjs/plugin/utc');
+const timezone = require('dayjs/plugin/timezone');
 
-console.log(dayjs().day());
+dayjs.extend(utc)
+dayjs.extend(timezone)
+
+console.log(dayjs.tz.guess());
+
+
+function updateClock() {
+    const now = dayjs().format('HH:mm:ss'); // get current time
+    document.getElementById('clock').textContent = now; // update DOM
+}
+updateClock(); // initial call
+setInterval(updateClock, 1000); // update every second
