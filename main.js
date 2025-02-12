7d4840cf6b7e69d16936d89eaa8683112130aa27
Fridgemann <120120633+Fridgemann@users.noreply.github.com>
2025-02-13 01:11:00 +0300

    finished

--- main.js
+++ main.js
@@ -5,12 +5,18 @@ const timezone = require('dayjs/plugin/timezone');
 dayjs.extend(utc)
 dayjs.extend(timezone)
 
-console.log(dayjs.tz.guess());
+document.getElementById('timezone').textContent = dayjs.tz.guess();
 
 
 function updateClock() {
     const now = dayjs().format('HH:mm:ss'); // get current time
     document.getElementById('clock').textContent = now; // update DOM
+
+    let d = new Date();
+    let day = dayjs(d).format('dddd, D MMMM, YYYY');
+    document.getElementById('date').textContent = day;
 }
+
 updateClock(); // initial call
 setInterval(updateClock, 1000); // update every second
+
